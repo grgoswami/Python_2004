f9970e789ecf767776a2bae6ff67d813a0207621
Gopi Goswami <grgoswami@gmail.com>
2020-04-22 14:39:59 -0400

    Replaced return with print

--- source/problem1.py
+++ source/problem1.py
@@ -1,10 +1,9 @@
 # Donuts.
-# Given an int count of a number of donuts, return a string
+# Given an int count of a number of donuts, print a string
 # of the form 'Number of donuts: <count>', where <count> is the number
 # passed in. However, if the count is 10 or more, then use the word 'many'
 # instead of the actual count.
-# So donuts(5) returns 'Number of donuts: 5'
-# and donuts(23) returns 'Number of donuts: many'
+# So donuts(5) prints 'Number of donuts: 5'
+# and donuts(23) prints 'Number of donuts: many'
 def donuts(count):
   # Write your code below
-  return
