a6ef1d4e8864afaf165c848e364c66249c92561e
Gopi Goswami <grgoswami@gmail.com>
2020-04-21 20:15:38 -0400

    Updated in class

--- source/lesson5.py
+++ source/lesson5.py
@@ -15,9 +15,9 @@ random.shuffle(students)
 print('\nThe students after shuffling:')
 print(students)
 
-
 # Here for and in are keywords, they are in purple
 # The syntax: for something in something:
+# Here we are iterating over a list, also called list iteration
 for st in students:
     print('This prestigious bachelors degree is awarded to: ' + st)
     
@@ -27,7 +27,39 @@ for ss in students:
 for tt in students:
     print('Note: ' + tt + ' gets this degree')
     
+# Range for loop
+list(range(1, 10))
+list(range(10))
+list(range(-2, 4))
+
+# This is called iterating over a range
+for ir in range(5):
+    print('This iteration is ' + str(ir))
+    
+# Iterating over a dictionary
+favorite = {
+'Ari': 'Dosa',
+'Anya': 'Salad',
+'Gia': 'Cheese',
+'Yuvi': 'Samosa', 
+'Shraddha': 'Pau Bhaji', 
+'Tista': 'Ice cream',
+'Auyona': 'Phoochka', 
+'Evaan': 'Samosa'
+}
+
+# '.' is a operator that's used to call functions on a 'class' or 
+# from a 'module' or an 'object' of a 'class'
+# Note: the print function can take multiple arguments
+for Name, Food in favorite.items():
+    print(Name, Food)
+    
+for name, food in favorite.items():
+    print('The favorite food of ' + name + ' is ' + food)
     
     
+    
+
+
     
     
