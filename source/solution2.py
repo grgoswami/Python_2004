11f1664c9ba3045a22078951faada802db22c6c5
Gopi Goswami <grgoswami@gmail.com>
2020-04-27 20:56:09 -0400

    Initial commit

---
+++ source/solution2.py
@@ -0,0 +1,42 @@
+# Use the following favorites dictionary below and answer the questions below
+
+favorites = {
+'Ari': ['Dosa', 'Gol gappe', 'Paan', 'Roti'],
+'Anya': ['Salad', 'Cheese'],
+'Gia': ['Cheese', 'Cookies', 'Goat masala'],
+'Yuvi': ['Samosa', 'Poori roti', 'Poori'], 
+'Shraddha': ['Pau Bhaji', 'Pani puri', 'Naan'], 
+'Tista': ['Ice cream', 'Cookies'],
+'Auyona': ['Phoochka', 'Sandesh'], 
+'Evaan': ['Samosa', 'Briyani', 'Cookies']
+}
+
+#1. What's Tista's second favorite food, print it?
+# Write your code below
+# print(favorites["Tista"])
+print(favorites["Tista"][1])
+
+#2. How many favorite foods does Shraddha have, print it?
+# Hint: use the len() function that I used in class
+# Write your code below
+# print(favorites['Shraddha'])
+print(len(favorites['Shraddha']))
+
+#3. Use a for loop to iterate over the dictionary and count the number of
+# favorite foods that each of you have and print like this:
+#
+# Ari has 4 favorite foods
+# Anya has 2 favorite foods
+# ...
+# Write your code below
+for name, foods in favorites.items():
+    print(name + ' has ' + str(len(foods)) + ' favorite foods')
+
+#4. Special credit (HARD): Iterate over the dictionary favorites and print the
+# name of the person if she/he has 'Chesse' as their favorite food. 
+# Hint: You need to use a for loop for dictionary and then a for loop for 
+# lists inside the first for loop and then an if statement to check
+# for equality using ==, as we saw in class yesterday.
+# Write your code below
+
+
