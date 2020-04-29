7b2552ed8505ecc37945fc8e747903355c128864
Gopi Goswami <grgoswami@gmail.com>
2020-04-29 18:57:11 -0400

    Initial commit

---
+++ source/problem3.py
@@ -0,0 +1,14 @@
+# Write a class called GroceryStore with a method called buy. 
+# It should behave like the following:
+
+# store = GroceryStore('Trader Joes', 'Millburn')
+# store.buy('Cookies', 3)
+# store.buy('Tomatoes', 4)
+
+# The above should print:
+# 
+# Creating a GroceryStore object called Trader Joes in Millburn
+# You bought 3 Cookies
+# You bought 4 Tomatoes
+#
+
