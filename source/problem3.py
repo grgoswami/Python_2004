61641394958fd7be0b3eace52a3b6bc234b50f97
Gopi Goswami <grgoswami@gmail.com>
2020-04-29 20:31:05 -0400

    Some edits

--- source/problem3.py
+++ source/problem3.py
@@ -1,14 +1,20 @@
 # Write a class called GroceryStore with a method called buy. 
-# It should behave like the following:
+# It should behave like the following or it should used in the
+# following way:
+
+# The following usage or behavior of the class GroceryStore is
+# also called an interface or inteded beahvior.
 
 # store = GroceryStore('Trader Joes', 'Millburn')
 # store.buy('Cookies', 3)
 # store.buy('Tomatoes', 4)
+# store.show_my_bill('Cookies', 3, 'Tomatoes', 4)
 
 # The above should print:
 # 
 # Creating a GroceryStore object called Trader Joes in Millburn
 # You bought 3 Cookies
 # You bought 4 Tomatoes
-#
+# You bill is: 3 + 4
+
 
