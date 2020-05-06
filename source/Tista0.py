832fde3e8aa6afd152d2f335c0a62e033b475a38
Gopi Goswami <grgoswami@gmail.com>
2020-05-05 20:50:10 -0400

    Initial commit

---
+++ source/Tista0.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 18:59:57 2020
+
+@author: gopi
+"""
+
+
+The following Capstone project ideas are inspired by:
+
+    
+
+https://papersowl.com/blog/best-capstone-project-topic-ideas
+
+    
+
+1. How to motivate students
+
+
+
+2. The importance of adjusting how to assess student learning
+
+
+
+3. The role of virtual classroom management in improving student behavior
+
+
+
+Your goal for this problem set is to take one of the topics above and 
+
+think about three concrete steps to solve any of the above problems.
+
+
+
+Our final goal is to take some of your unique ideas, refine them and
+
+build a real project using Python (and or other tools). May be some day
+
+you'll start your own company when one of your ideas grow big!
+
+
+
+You've already showed us the basics and how to use them in things like classifying age and how people
+
+use them in g-mail and stuff. But I think that there are other ways to use them like in games. In other
+
+words I did some research and I think that there's a way that you can play games on python using only
+
+the things you taught and showed us. I think that there is a way to improve your problem sets in a way that 
+
+will encourage students to do them. I left an example of what I mean about the problem sets below. I think making
+
+these type of problem sets gives students more to play with. If you dont understand something please e-mail me.
+
+
+
+Example : Mad Libs Using Python (Because this is an example I'm only going to do a little )
+
+
+
+name = input('Enter your name')
+age = int(input('Enter your age'))
+
+thing1 = input ('Enter something you like to do')
+
+thing2 = input ('Enter something else you like to do')
+
+place = input ('Enter a place that you either want to go to or a place that you like going to')
+
+thing_to_do = input ('Enter something you want to do or like doing in that place')
+
+friend =  input ('Enter the name of one of your friends')
+
+food = input ('Enter your favorite food')
+
+food1 = input ('Enter another one of your favorite foods')
+
+
+
+
+
+print ('hi ' + name)
+print (name + ' is ' + str(age) + ' years old')
+print (name + ' likes to ' + thing1 + ' and ' + thing2 )
+
+print ('One day ' + name +' ' + friend + ' went to ' + place)
+
+print ('In ' + place + name + 'and' + friend + ' decided to ' + thing_to_do)
+
+print ('For lunch they had ' + food + 'and' + food1 )
+
+#... You could just keep going and make a story
---
+++ source/ap0.py
@@ -0,0 +1,9 @@
+
+name = input('Enter your name:') 
+print(name)
+
+drink = input('What would you like to drink: ')
+print(drink)
+
+print (name + ' would like to drink ' + drink)
+
