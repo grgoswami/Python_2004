126e3a2dc833e07325b0cb25c8561d5c0b6acdd8
Gopi Goswami <grgoswami@gmail.com>
2020-05-07 21:18:16 -0400

    Initial commit

---
+++ source/problem6.py
@@ -0,0 +1,14 @@
+
+def pig_latin(word):
+    """
+    Arguments
+    ----------
+    word : string
+        
+    Returns
+    -------
+    All but the first letter of the word + the first word + 'ly'.
+    Hint: All you'll need are slicing and string concatenation
+    Write your code below
+    """
+    
