22b51a9b3d12572c7a9a3753d8242565996cf1d8
Gopi Goswami <grgoswami@gmail.com>
2020-05-11 20:16:28 -0400

    Initial commit

---
+++ source/regular1.py
@@ -0,0 +1,29 @@
+
+price = 100.5
+price * 10
+print(_)
+
+print(price)
+price + 10
+print(_)
+
+round(13.6466, 3)
+round(13.6462, 2)
+round(13.6462, 1)
+round(13.6462)
+
+round(13.64, 1)
+round(13.64)
+
+
+print("It's")
+print("It's")
+print('"Hello", is what she said.')
+
+print('"Isn\'t," they said.')
+print('"Isn\'t," they said.\n\n\n\n\n\n')
+print('"Isn\'t," they said.')
+print("\"Hello\"")
+
+print("Hello\t\t\tWorld!")
+
