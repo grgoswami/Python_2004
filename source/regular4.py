a2f7bec61b46793b64765b046939aac50ff5bfa9
Gopi Goswami <grgoswami@gmail.com>
2020-05-20 21:11:29 -0400

    Initial commit

---
+++ source/regular4.py
@@ -0,0 +1,28 @@
+
+[1, 10, 100] + [-1, -10, -100]
+
+[1, 10, 100] + [1, 10, 100]
+
+[1, 10, 100] + 'string'
+
+'string' + [1, 10, 100]
+
+3 + [1, 10, 100]
+
+[1, 10, 100] + 4.5
+
+list_concatenation([1, 10, 100], [1, 10, 100])
+
+string_concatenation('word', 'letter')
+
+# polymorphism
+
+list0 = [1, 10, 100]
+print(list0)
+
+list0[1] = 1000
+print(list0)
+
+str0 = 'Millburn'
+str0[1] = 't'
+str0[0] + 't' + str0[2:] 
