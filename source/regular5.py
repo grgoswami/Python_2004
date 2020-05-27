da7d02991475845333cb189c3427ba1fb791e304
Gopi Goswami <grgoswami@gmail.com>
2020-05-27 19:54:30 -0400

    Initial commit

---
+++ source/regular5.py
@@ -0,0 +1,46 @@
+
+cubes = [1, 8, 27, 65, 125]
+print(cubes)
+
+cubes[3]
+
+cubes[3] = 64
+print(cubes)
+
+cubes.append(1000)
+print(cubes)
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+print(letters)
+
+letters[2:5]
+
+letters[2:5] = ['C', 'D', 'E']
+print(letters)
+
+letters[2:5] = ['CCC', 'DDD']
+print(letters)
+
+letters[2:5] = ['CCCCCCC']
+print(letters)
+
+letters[2:5] = ['CCCCCCC', 'D', 'DD', 'DDD', 'DDDDD']
+print(letters)
+
+a = ['a', 'b', 'c']
+print(a)
+n = [1, 2, 3]
+print(n)
+x = [a, n]
+print(x)
+
+print(x[1])
+print(x[0])
+
+print(x[1])
+
+print(x[0][1])
+
+
+
+
