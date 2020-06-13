be3c640cdbdcf069c6d51eb8696ee0896082ba67
Gopi Goswami <grgoswami@gmail.com>
2020-06-12 21:46:32 -0400

    Initial commit

---
+++ source/Shradha0.py
@@ -0,0 +1,42 @@
+# a collection in instructions
+# a collection of code
+def function1():
+    print('ahhhh!')
+    print('ahhhhhh 2')
+print('this is outside function')
+
+# a mapping
+# input or an argument
+def function2(x):
+    return 2*x
+a = function2(3)
+# return value or output
+print(a)
+
+b = function2(4)
+print (b)
+
+c = function2(5)
+print(c)
+
+d = function2(9)
+
+def function3(x,y):
+    return x + y
+
+e = function3 (1,2)
+print(e)
+
+def function4 (x):
+    print(x)
+    print('still in this function')
+    return 3*x
+
+f = function4(4)
+print(f)
+
+def function5(some_argument):
+    print(some_argument)
+    print('weeeeeee')
+    
+
