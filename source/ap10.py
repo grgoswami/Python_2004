7fc1ab717734ea3b48068fa665a312f5c079b5ad
Gopi Goswami <grgoswami@gmail.com>
2020-06-24 20:00:09 -0400

    Fixed run path

--- source/ap10.py
+++ source/ap10.py
@@ -1,6 +1,12 @@
+
+import os
+os.chdir('/home/gopi/Python_202004/source')
+import sys
+sys.path.append('/home/gopi/Python_202004/source')
+
 import reader
 
 # Designing the 'interface' to solve the problem that we have 
 reader0 = reader.XSV_Reader(separator=',')
-data = reader0.read(r'/home/gopi/Python_202004/data/corona/ourworldindata/full_data.csv')
+data = reader0.read(r'/home/gopi/Python_202004/data/corona/ourworldindata/test_data.csv')
 data.head()
