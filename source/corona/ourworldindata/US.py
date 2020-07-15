6e3d4ed989b13ca48957f77af992485216650d62
Gopi Goswami <grgoswami@gmail.com>
2020-07-14 20:03:40 -0400

    Slightly rearranged

--- source/corona/ourworldindata/US.py
+++ source/corona/ourworldindata/US.py
@@ -1,23 +1,25 @@
 
 import pandas as pd
-import seaborn 
+import seaborn as sb
 import matplotlib.pyplot as plt
 
 pd.set_option('display.max_rows', 5000)
 pd.set_option('display.max_columns', 1000)
 pd.set_option('display.width', 5000)
 
-seaborn.set_style('darkgrid')
+sb.set_style('darkgrid')
 
 """
 https://ourworldindata.org/coronavirus-source-data
 """
 
 full_data = pd.read_csv(r'/home/gopi/Python_202004/data/corona/ourworldindata/full_data.csv')
-print(full_data.head())
+print(full_data.shape)
+print(full_data.head(10))
+print(full_data.tail())
+
 print(full_data.describe())
 print(full_data.location.value_counts())
-print(full_data.shape)
 
 US = full_data.loc[full_data.location == 'United States',:]
 print(US.shape)
