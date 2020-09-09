223ad82907b46493a3b9a9fb6ef9311a781d1b44
Gopi Goswami <grgoswami@gmail.com>
2020-09-08 20:12:17 -0400

    Added comments

--- source/toll_booth.py
+++ source/toll_booth.py
@@ -11,5 +11,15 @@ class Vehicle:
             self.kind, self.make, self.color))    
 
 class TollBooth:
-    pass
-
+    def __init__(self):
+        self.toll_charge = {
+            'Sedan': 1.0,
+            'SUV': 2.0,
+            'Truck': 3.0,
+            'Sports car': 5.0}        
+    
+    def collect_toll(self, vehicle):
+       self.vehicle = vehicle
+       print('In collect_toll: kind={0}, toll_charge={1}'.format(self.vehicle.kind,
+                                                                 self.toll_charge[self.vehicle.kind]))
+        
--- source/toll_booth_example.py
+++ source/toll_booth_example.py
@@ -1,4 +1,5 @@
-
+import os
+os.chdir(r'/home/gopi/Python_202004/source')
 import toll_booth as tb
 
 # Vehicle is a class
@@ -17,6 +18,27 @@ list_of_vehicles = [v0, v1, v2, v3]
 for vh in list_of_vehicles:
     vh.describe()
     
+print(v0.kind)
+print(v0.make)
+print(v3.color)
+    
+tb0 = tb.TollBooth()
+tb0.collect_toll(v0)
+tb0.collect_toll(v1)
+tb0.collect_toll(v0)
+tb0.collect_toll(v2)
+tb0.collect_toll(v3)
+tb0.collect_toll(v0)
+
+list_of_vehicles = [v0, v1, v2, v3, v0, v2, v1, v3, v0]
+for vh in list_of_vehicles:
+    tb0.collect_toll(vh)
+    
+    
+
+
+
+
 
 
 
